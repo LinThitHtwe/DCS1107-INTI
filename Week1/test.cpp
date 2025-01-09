30d3d6f65ce10fe8cd4938385ae4c066993383aa
LinThitHtwe <linthit2745@gmail.com>
2025-01-09 16:12:07 +0800

    [add] test.cpp week 1

---
+++ Week1/test.cpp
@@ -0,0 +1,10 @@
+// preprocessor directory ( header file ) --> #include <iostream>
+#include <iostream>
+using namespace std;
+
+main()
+{	
+	cout << "Hi :3 \n";
+	cout << "This is new lineeeeee\n";
+	cout << "\t new tabbb";
+}
